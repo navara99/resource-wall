27e8a7b2b11059338dba2bd15321f5ca05a9689a
Tharsikan <thar.navaratnam@gmail.com>
2021-11-27 23:45:09 +0000

    fix bug with ON DELETE CASCADE

--- db/schema/03_resources.sql
+++ db/schema/03_resources.sql
@@ -3,13 +3,15 @@ DROP TABLE IF EXISTS resources CASCADE;
 CREATE TABLE resources
 (
   id SERIAL PRIMARY KEY NOT NULL,
-  user_id INTEGER REFERENCES users(id) DELETE ON CASCADE,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   title TEXT NOT NULL,
   description TEXT NOT NULL,
   url TEXT NOT NULL,
   media_url TEXT NOT NULL,
   is_video BOOLEAN NOT NULL,
-  category_id INTEGER REFERENCES categories(id),
+  category_id INTEGER REFERENCES categories(id) ON DELETE CASCADE,
   is_public BOOLEAN NOT NULL
 );
+
+
 
