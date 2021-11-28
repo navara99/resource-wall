acf143c78894159dec995455f9961f2739cf6c4a
Tharsikan <thar.navaratnam@gmail.com>
2021-11-28 23:38:53 +0000

    add seeds for categories table

---
+++ db/seeds/02_categories.sql
@@ -0,0 +1,15 @@
+INSERT INTO categories (type)
+VALUES
+  ('Art/DIY'),
+  ('Auto and Vehicles') ,
+  ('Entertainment'),
+  ('Film and Animation'),
+  ('Gaming'),
+  ('Music'),
+  ('News and Politics'),
+  ('Pets and animals'),
+  ('Sports'),
+  ('Technology'),
+  ('Travelling'),
+  ('Other')
+ ;
