0a4878c7cdba0306283eb5b0a5b0c8f9aa0cc05a
Tharsikan <thar.navaratnam@gmail.com>
2021-11-28 20:31:27 +0000

    add /api/resources route

--- server.js
+++ server.js
@@ -40,12 +40,12 @@ app.use(express.static("public"));
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
-const widgetsRoutes = require("./routes/widgets");
+const resourcesRoutes = require("./routes/resources");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
-app.use("/api/widgets", widgetsRoutes(db));
+app.use("/api/resources", resourcesRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
 // Home page
