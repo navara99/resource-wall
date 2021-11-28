9d84910980c16c4491f500a53cfb392afd215bcf
Tharsikan <thar.navaratnam@gmail.com>
2021-11-28 17:57:54 +0000

    add updateUser function

--- db/query-helpers.js
+++ db/query-helpers.js
@@ -1,3 +1,5 @@
+const { query } = require("express");
+
 const defaultProfilePicUrl = 'https://t4.ftcdn.net/jpg/00/64/67/63/360_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg';
 
 const queryGenerator = (db) => {
@@ -9,10 +11,10 @@ const queryGenerator = (db) => {
   // };
 
   const getUserByValue = async (columnName, value) => {
-    const values = [columnName, value];
-    const queryString = `SELECT * FROM users WHERE $1 = $2;`
+    const values = [value];
+    const queryString = `SELECT * FROM users WHERE ${columnName} = $1;`;
     const result = await db.query(queryString, values);
-    if (!result.image_url) result.image_url = defaultProfilePicUrl;
+    // if (!result.image_url) result.image_url = defaultProfilePicUrl;
     return result;
   };
 
@@ -28,7 +30,25 @@ const queryGenerator = (db) => {
     return await db.query(queryString, values);
   }
 
-  return { createNewUser, getUserByValue };
+  const updateUser = async (newUserInfo) => {
+    const {  firstName, lastName, username, email, bio, userId } = newUserInfo;
+    const values = [firstName, lastName, username, email, bio, userId];
+
+    const queryString = `
+    UPDATE users
+    SET first_name = $1,
+    last_name = $2,
+    username = $3,
+    email = $4,
+    bio = $5
+    WHERE id = $6
+    RETURNING *;
+    `;
+
+    return await db.query(queryString, values);
+  }
+
+  return { createNewUser, getUserByValue, updateUser };
 }
 
 module.exports = queryGenerator;
