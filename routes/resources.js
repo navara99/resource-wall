7a94dd2a7d5e26c1e432783ee7c9ce354751adb6
Tharsikan <thar.navaratnam@gmail.com>
2021-11-28 23:37:31 +0000

    complete POST handler for /resources

--- routes/resources.js
+++ routes/resources.js
@@ -3,24 +3,25 @@ const router = express.Router();
 const queryGenerator = require("../db/query-helpers");
 
 module.exports = (db) => {
-  const { addNewResource } = queryGenerator(db);
+  const { addNewResource, getIdFromCategory } = queryGenerator(db);
 
   router.get("/", (req, res) => {
 
   });
 
   router.post("/", async (req, res) => {
-    const user_id = req.session.user_id;
-    let { is_private } = req.body;
-
-    is_private = is_private ? true : false;
-
-    const newResourceInput = { ...req.body, is_private, user_id }
-    const newResource = await addNewResource(newResourceInput);
-
-    console.log(newResource);
-
-    res.json(newResource);
+    try {
+      const user_id = req.session.user_id;
+      let { is_private, category } = req.body;
+      
+      is_private = is_private ? true : false;
+      const category_id = await getIdFromCategory(category);
+      const newResourceInput = { ...req.body, is_private, user_id, category_id }
+      const newResource = await addNewResource(newResourceInput);
+      res.json(newResource);
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
   })
 
   return router;
