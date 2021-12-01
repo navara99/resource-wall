b9cf49b1f07bab0851abb24d4afc19358fb4eafe
Siu <siuc@protonmail.com>
2021-12-01 01:09:36 +0000

    attach information to average rating (resource details)

--- public/scripts/components/resource-details.js
+++ public/scripts/components/resource-details.js
@@ -5,15 +5,21 @@ const compileComments = (resourceDetails) => {
     if (!comment) {
       comments.push({ comment, username, timestamp });
     }
-
   }
   return comments;
 };
 
 const getHostname = (url) => {
-  const parser = document.createElement('a');
+  const parser = document.createElement("a");
   parser.href = url;
   return parser.hostname;
+};
+
+const displayRating = (rating, numOfRating) => {
+  const displayStr = rating
+    ? `${rating} (Based on ${numOfRating} ratings)`
+    : "No rating yet";
+  return displayStr;
 };
 
 const updateResourceDeails = () => {
@@ -22,17 +28,21 @@ const updateResourceDeails = () => {
   const $mediaDisplayedURL = $("#details-display-link-on-media");
   const $title = $("#details-title");
   const $link = $("#details-link");
+  const $rating = $("#details-rating");
 
   return (resourceDetails) => {
-    const { description, url, title } = resourceDetails[0];
+    const { description, url, title, rating, number_of_rating } =
+      resourceDetails[0];
     const comments = compileComments(resourceDetails);
     const hostname = getHostname(url);
+    const ratingText = displayRating(rating, number_of_rating);
 
     $mediaDisplayedURL.text(hostname);
     $descriptions.text(description);
     $mediaURL.attr("href", url);
     $link.attr("href", url);
     $link.text(hostname);
+    $rating.text(ratingText);
     $title.text(title);
   };
 };
