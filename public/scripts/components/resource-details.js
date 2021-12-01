69444a5d30d7ddafe7bce6d06c2bbc694f15f19e
Siu <siuc@protonmail.com>
2021-12-01 01:03:35 +0000

    attach information to  the title and links(resource details)

--- public/scripts/components/resource-details.js
+++ public/scripts/components/resource-details.js
@@ -20,14 +20,19 @@ const updateResourceDeails = () => {
   const $descriptions = $("#details-desciption");
   const $mediaURL = $("#details-link-on-media");
   const $mediaDisplayedURL = $("#details-display-link-on-media");
+  const $title = $("#details-title");
+  const $link = $("#details-link");
 
   return (resourceDetails) => {
-    const { description, url } = resourceDetails[0];
+    const { description, url, title } = resourceDetails[0];
     const comments = compileComments(resourceDetails);
     const hostname = getHostname(url);
 
     $mediaDisplayedURL.text(hostname);
     $descriptions.text(description);
     $mediaURL.attr("href", url);
+    $link.attr("href", url);
+    $link.text(hostname);
+    $title.text(title);
   };
 };
