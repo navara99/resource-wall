b85bcc6ae5ce9941018ebcc57687cef0b7e25800
Siu <siuc@protonmail.com>
2021-12-22 17:41:27 -0500

    refactor

--- public/scripts/components/user-page.js
+++ public/scripts/components/user-page.js
@@ -12,29 +12,19 @@ const updateUserFunctionGenerator = () => {
 
   return async (id) => {
     try {
-      if (id) {
-        getUserDetails(id).then((userInfo) => {
-          $editMyProfile.hide();
-          const { username, first_name, last_name, profile_picture_url, bio } =
-            userInfo;
-          $username.text(`@${username}`);
-          $name.text(`${first_name} ${last_name}`);
-          $bio.text(bio);
-          $("#user-profile-picture").attr("src", profile_picture_url);
-          updateView("userPage");
-        });
-      } else {
-        getMyDetails().then((userInfo) => {
-          $editMyProfile.show();
-          const { username, first_name, last_name, profile_picture_url, bio } =
-            userInfo;
-          $username.text(`@${username}`);
-          $name.text(`${first_name} ${last_name}`);
-          $bio.text(bio);
-          $("#user-profile-picture").attr("src", profile_picture_url);
-          updateView("userPage");
-        });
-      }
+      const { username, first_name, last_name, profile_picture_url, bio } = id
+        ? await getUserDetails(id)
+        : await getMyDetails();
+
+      if (id) $editMyProfile.hide();
+      if (!id) $editMyProfile.show();
+
+      $username.text(`@${username}`);
+      $name.text(`${first_name} ${last_name}`);
+      $bio.text(bio);
+      $("#user-profile-picture").attr("src", profile_picture_url);
+      updateView("userPage");
+
     } catch (err) {
       updateError(err);
     }
