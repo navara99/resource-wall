1f11be4def1b928fdd3b503a0e117fc43bbb2d76
Tharsikan <thar.navaratnam@gmail.com>
2021-12-23 20:23:23 +0000

    append edit form to modal

--- public/scripts/components/edit-resource.js
+++ public/scripts/components/edit-resource.js
@@ -1,7 +1,9 @@
 const showEditResourceModal = async (resourceId) => {
-  const resourceDetails = await getDetailsOfResources(resourceId);
+  const [resourceDetails] = await getDetailsOfResources(resourceId);
   const { title, url, description, is_private, category_id } = resourceDetails;
   const $editResourceForm = $("#edit-resource-form");
+  const $editModalContent = $("#edit-form-modal");
+  $editModalContent.append($editResourceForm);
 
   console.log(resourceId);
   console.log(resourceDetails);
