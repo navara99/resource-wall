0578aeedd9804a7c67fc0e7f9e2f4cd84367234f
Tharsikan <thar.navaratnam@gmail.com>
2021-12-23 19:33:31 +0000

    add template for modal

--- public/scripts/components/edit-resource.js
+++ public/scripts/components/edit-resource.js
@@ -1,9 +1,26 @@
-
+const createEditModal = () => {
+  const $modal = $(`
+  <div id="${id}-confirm-delete" class="modal">
+    <a href="#!" id="close-confirm-delete" class="modal-close  waves-effect waves-light btn-flat">
+      <i class="material-icons right">close</i>
+    </a>
+    <div class="modal-content">
+      <h4>Are you sure?</h4>
+      <p>Are you sure you want to delete ${title}?</p>
+    </div>
+    <div class="modal-footer">
+      <a href="#!" id="${id}-delete" class="modal-close  waves-effect waves-light red btn">Confirm</a>
+  </div>
+</div>
+`);
+  return $modal;
+}
 
 
 const showEditResourceModal = async (resourceId) => {
   const resourceDetails = await getDetailsOfResources(resourceId);
   const { title, url, description, is_private, category_id } = resourceDetails;
+
 
   console.log(resourceId);
   console.log(resourceDetails);
