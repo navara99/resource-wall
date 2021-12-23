d46895d91493f6283a03b630ca041bad8e2d8858
Tharsikan <thar.navaratnam@gmail.com>
2021-12-23 20:07:18 +0000

    move editModal to another file

--- public/scripts/components/edit-resource.js
+++ public/scripts/components/edit-resource.js
@@ -1,33 +1,10 @@
-const createEditModal = () => {
-  const $editResourceForm = $("#edit-resource-form");
-
-  const $modal = $(`
-  <div id="${id}-edit" class="modal">
-    <a href="#!" id="close-edit" class="modal-close  waves-effect waves-light btn-flat">
-      <i class="material-icons right">close</i>
-    </a>
-    <div class="modal-content">
-      <h4>Edit ${title}</h4>
-    </div>
-    <div class="modal-footer">
-      <a href="#!" id="${id}-delete" class="modal-close  waves-effect waves-light red btn">Confirm</a>
-  </div>
-</div>
-`);
-
-
-  return $modal;
-}
-
-
 const showEditResourceModal = async (resourceId) => {
   const resourceDetails = await getDetailsOfResources(resourceId);
   const { title, url, description, is_private, category_id } = resourceDetails;
-
+  const $editResourceForm = $("#edit-resource-form");
 
   console.log(resourceId);
   console.log(resourceDetails);
-
 };
 
 
