1a26e00776ac857bb6c0b3a9c487957fa0c85ae7
Tharsikan <thar.navaratnam@gmail.com>
2021-12-23 19:32:24 +0000

    destructure editable info from resourceDetails

--- public/scripts/components/edit-resource.js
+++ public/scripts/components/edit-resource.js
@@ -1,7 +1,13 @@
+
+
+
 const showEditResourceModal = async (resourceId) => {
   const resourceDetails = await getDetailsOfResources(resourceId);
+  const { title, url, description, is_private, category_id } = resourceDetails;
+
   console.log(resourceId);
   console.log(resourceDetails);
+
 };
 
 
