12d301974b5c545638229ec54a0c56e5fb2a7585
Tharsikan <thar.navaratnam@gmail.com>
2021-12-23 21:32:16 +0000

    prefill category select

--- public/scripts/components/edit-resource.js
+++ public/scripts/components/edit-resource.js
@@ -3,19 +3,22 @@ const placeInput = (elem, value) => {
   elem.val(value);
 };
 
-const updateEditForm = (title, description, url) => {
+const updateEditForm = (title, description, url, is_private, category) => {
   placeInput($("#title-edit"), title);
   placeInput($("#description-edit"), description);
   placeInput($("#url-edit"), url);
+  placeInput($("#category-edit"), category);
+  $("#category-edit").formSelect();
 };
 
 const showEditResourceModal = async (resourceId) => {
   const [resourceDetails] = await getDetailsOfResources(resourceId);
-  const { title, url, description, is_private, category_id } = resourceDetails;
+  const { title, url, description, is_private, category_id, catergory } = resourceDetails;
   const $editResourceForm = $("#edit-resource-form");
   const $editModalContent = $(`#${resourceId}-edit-form-modal`);
   $editModalContent.append($editResourceForm);
-  updateEditForm(title, description, url, is_private, category_id);
+  console.log(catergory);
+  updateEditForm(title, description, url, is_private, catergory);
   $editResourceForm.show();
   console.log(resourceId);
   console.log(resourceDetails);
