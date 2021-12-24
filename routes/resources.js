81806080f4d41a0fc459e7e669158529b20801ca
Tharsikan <thar.navaratnam@gmail.com>
2021-12-24 02:42:14 +0000

    remove omebed func

--- routes/resources.js
+++ routes/resources.js
@@ -7,23 +7,6 @@ const { generateMedia } = require("./routeHelpers/mediaHelpers");
 const apiKey = process.env.IFRAME_KEY;
 const providers = require("./json/providers.json");
 
-const omebed = (url) => {
-  for (const provider of providers) {
-    const { endpoints, provider_url } = provider;
-    const { schemes, url: oembedURL } = endpoints[0];
-    const urlRegex2 = new RegExp(provider_url + "*");
-    const matched = urlRegex2.test(url);
-    if (matched) return oembedURL;
-    if (schemes) {
-      for (const str of schemes) {
-        const urlRegex = new RegExp(str);
-        const match = urlRegex.test(url);
-        if (match) return oembedURL;
-      }
-    }
-  }
-};
-
 module.exports = (db) => {
   const {
     addNewResource,
