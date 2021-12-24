27ab0b0b0ff49e2b2ae79e9c75bb085d6111769b
Tharsikan <thar.navaratnam@gmail.com>
2021-12-24 02:14:08 +0000

    move omebed function to mediaHelpers.js

---
+++ routes/routeHelpers/mediaHelpers.js
@@ -0,0 +1,18 @@
+
+
+const omebed = (url) => {
+  for (const provider of providers) {
+    const { endpoints, provider_url } = provider;
+    const { schemes, url: oembedURL } = endpoints[0];
+    const urlRegex2 = new RegExp(provider_url + "*");
+    const matched = urlRegex2.test(url);
+    if (matched) return oembedURL;
+    if (schemes) {
+      for (const str of schemes) {
+        const urlRegex = new RegExp(str);
+        const match = urlRegex.test(url);
+        if (match) return oembedURL;
+      }
+    }
+  }
+};
