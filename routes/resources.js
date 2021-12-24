968d26e89eab0b3989789cf6c8c5fd091aa1f3ca
Tharsikan <thar.navaratnam@gmail.com>
2021-12-24 23:49:42 +0000

    move uploads folder into public folder

--- routes/resources.js
+++ routes/resources.js
@@ -7,7 +7,7 @@ const { generateMedia, omebed } = require("./routeHelpers/mediaHelpers");
 const multer = require("multer");
 const storage = multer.diskStorage({
   destination: function (req, res, cb) {
-    cb(null, "./uploads/");
+    cb(null, "./public/uploads/");
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
